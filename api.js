f29758b1e2cef167ce5d6fd64fb80593dce3c056
onedrako <onedrako@gmail.com>
2022-09-25 22:14:26 -0500

    Clase 126. Agregando PUT, PATCH y DELETE

--- api.js
+++ api.js
@@ -11,6 +11,20 @@ app.post('/', (req, res) => {
   res.status(201).send('Creando chanchito');
 });
 
+app.get('/:id', (req, res) => {
+  console.log(req.params);
+  res.status(200).send(req.params);
+});
+
+app.put('/:id', (req, res) => {
+  console.log(req.params);
+  res.status(204)
+});
+
+app.delete('/:id', (req, res) => {
+  res.status(204)
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
