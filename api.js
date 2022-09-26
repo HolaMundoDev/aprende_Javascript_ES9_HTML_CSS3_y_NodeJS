887d39db131408ca4955e38b958fb777defec3f9
onedrako <onedrako@gmail.com>
2022-09-25 21:26:37 -0500

    Clase 125. Agregando endpoint POST

--- api.js
+++ api.js
@@ -7,7 +7,10 @@ app.get('/', (req, res) => {
   res.status(200).send('Chanchito feliz');
 });
 
+app.post('/', (req, res) => {
+  res.status(201).send('Creando chanchito');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
