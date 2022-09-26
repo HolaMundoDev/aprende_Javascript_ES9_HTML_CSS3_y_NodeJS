a1e9f0c6d0791c1a16714ea07306dbfe894de42a
onedrako <onedrako@gmail.com>
2022-09-26 00:41:47 -0500

    Clase 132. Buscar, actualizar y eliminar

--- index.js
+++ index.js
@@ -10,11 +10,56 @@ const User = moongose.model('User', {
 
 const crear = async () => {
   const user = new User({
-    userName: 'Chanchito Feliz',
-    edad: 15
+    userName: 'Chanchito Triste',
+    edad: 25
   });
   const savedUser = await user.save();
   console.log(savedUser);
 }
 
-crear();
+// crear()
+
+const buscarTodo = async () => {
+  const users = await User.find();
+  console.log(users);
+} 
+
+// buscarTodo();
+
+const buscar = async () => {
+  const user = await User.find({userName: 'Chanchito Feliz'});
+  console.log(user);
+}
+
+// buscar();
+
+const buscarUno = async () => {
+  const user = await User.findOne({userName: 'Chanchito Feliz'});
+  console.log(user);
+}
+
+// buscarUno();
+
+// const actualizar = async () => {
+//   const user = await User.findOneAndUpdate({userName: 'Chanchito Feliz'}, {edad: 30});
+//   console.log(user);
+// }
+
+const actualizar = async () => {
+  const user = await User.findOne({userName: 'Chanchito Feliz'});
+  console.log(user);
+  user.edad = 35;
+  await user.save(); 
+}
+
+// actualizar();
+
+const eliminar = async () => {
+  const user = await User.findOne({userName: 'Chanchito Triste'});
+  console.log(user);
+  if(user) {
+    await user.remove();
+  }
+}
+
+eliminar();
