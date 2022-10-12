f08fe354e43dc5fd38b84ae775cdb9befe72dcd3
onedrako <onedrako@gmail.com>
2022-10-12 17:59:15 -0500

    Clase 138 Cargando plantilla y formulario

--- app/main.js
+++ app/main.js
@@ -1,1 +1,34 @@
-console.log("Chanchito Feliz")
+const loadInitialTemplate = () => {
+  const template =  /*html*/`
+    <h1>Usuarios</h1>
+    <form id="user-form">
+      <div>
+        <label>Nombre</label>
+        <input name="name">
+      </div>
+      <div>
+        <label>Apellido</label>
+        <input name="lastname">
+      </div>
+      <button type="submit">Guardar</button>
+    </form>
+    <ul id="user-list"></ul>
+  `
+  const body = document.querySelector('body');
+  body.innerHTML = template;
+}
+
+const addFormListener = () => {
+  const userForm = document.getElementById("user-form")
+  userForm.onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(userForm);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+  }
+}
+
+window.onload = () => {
+  loadInitialTemplate();
+  addFormListener();
+}
